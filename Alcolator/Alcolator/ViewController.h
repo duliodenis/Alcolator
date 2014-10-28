c70fdfe774f9bcb7704a0d6fe06b342a7b0e6fc6
Dulio Denis <dulio.denis@yahoo.com>
2014-10-28 16:04:29 -0400

    Initial commit

---
+++ Alcolator/Alcolator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Alcolator
+//
+//  Created by Dulio Denis on 10/28/14.
+//  Copyright (c) 2014 ddApps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
